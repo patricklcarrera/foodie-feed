ea9c5ac69864221bd1b14df761103e9810acc0aa
Yeeun Jung <rjyeeun@gmail.com>
2023-02-14 12:43:34 -0500

    finished sinatra

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-
+#getting individual user
   get '/users/:id' do
     user = User.find(params[:id])
     user.to_json
   end
-
+#getting all users
   get '/users' do
     users = User.all
     users.to_json
@@ -21,11 +21,7 @@ class ApplicationController < Sinatra::Base
 #  password: "123"
 # }
 
-
-
-
-
-
+#new user sign-up 
   post '/users' do
     user = User.create!(username: params[:username], 
             password: params[:password], 
@@ -35,21 +31,22 @@ class ApplicationController < Sinatra::Base
             )
     user.to_json
   end
+#get all comments for one recipe
   get '/recipes/:id/comments' do
     comments = Recipe.find(params[:id]).comments
     comments.to_json
   end
-
+#get one recipe
   get '/recipes/:id' do
     recipe = Recipe.find(params[:id])
     recipe.to_json
   end
-
+#get all recipes
   get '/recipes' do
     recipes = Recipe.all
     recipes.to_json
   end
- 
+#create a new recipe (within the first user's account)
   post '/recipes/new' do
     recipe = Recipe.create!(name: params[:name],
                         description: params[:description],
@@ -61,12 +58,12 @@ class ApplicationController < Sinatra::Base
                         )
     recipe.to_json
   end
-
+#delete one recipe
   delete '/recipes/:id' do
     recipe = Recipe.find(params[:id])
     recipe.destroy
   end
-
+#write a new comment for the specific recipe
   post '/recipes/:id/comments/new' do
     recipe = Recipe.find(params[:id])
     comments = recipe.comments.create!(content: params[:content],
@@ -75,22 +72,30 @@ class ApplicationController < Sinatra::Base
     comments.to_json
   end
 
+#edit exiting comment
+ patch '/comments/:id/edit' do
+  comment = Comment.find(params[:id])
+  comment.update(content: params[:content])
+  comment.to_json
+ end
+#delete one comment
   delete '/comments/:id' do
     comment = Comment.find(params[:id])
     comment.destroy
   end
-
+#view saved recipes
   get '/saved_recipes' do
     recipe = Recipe.all.where(saved_recipe: 1)
     recipe.to_json
   end
-
+#delete one recipe list from the saved recipes list
   patch '/recipes/:id/saved_recipes/delete' do
     recipe = Recipe.find(params[:id])
     recipe.update(saved_recipe: 0)
     recipe.to_json
   end
 
+#add one recipe list from the saved recipes list
   patch '/recipes/:id/saved_recipes/add' do
     recipe = Recipe.find(params[:id])
     recipe.update(saved_recipe: 1)
