d9d14e8f90eb4003ddaa22d10ed649f21bca250a
Patrick <patricklcarrera@gmail.com>
2023-02-16 15:14:00 -0600

    bootstrappin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { NavLink, useNavigate } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
 
 export default function Header(){
 
 return (
-    <h1>
-        <h1>Foodie Feed</h1>
-            <NavLink to="/recipes"><button >All Recipes</button></NavLink>
-            <NavLink to='/recipes/new'><button>Add Recipe</button></NavLink>
-            <NavLink to='/savedrecipes'><button>My Saved Recipes</button></NavLink>
-            <NavLink to='/users/:id/recipes'><button>My Recipes</button></NavLink>
-
-    </h1>
+    <div>
+        <Navbar bg="light" expand="lg">
+             <Container align='center'>
+                    <Navbar.Brand href="/recipes">Foodie Feed</Navbar.Brand>
+                    <LinkContainer to="/recipes"><Nav.Link>All recipes</Nav.Link></LinkContainer>
+                    <LinkContainer to="/recipes/new"><Nav.Link>My Saved Recipes</Nav.Link></LinkContainer>
+                    <LinkContainer to="/savedrecipes"><Nav.Link>My Saved Recipes</Nav.Link></LinkContainer>
+                    <LinkContainer to="/users/:id/recipes"><Nav.Link>My Profile</Nav.Link></LinkContainer>
+                    {/* <Nav.Link href="/users/:id/recipes">My Profile</Nav.Link> */}
+            </Container>
+        </Navbar>
+    </div>
 )
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export default function Search({searchTerm, changeSearch}){
     
@@ -8,11 +9,15 @@ export default function Search({searchTerm, changeSearch}){
 
     return (
         <div>
-            <input
-                value={searchTerm}
-                type="text"
-                placeholder='Looking for a recipe?'
-                onChange={handleChange}/>
+            <Form>
+            <Form.Group>
+                <Form.Control
+                    value={searchTerm}
+                    type="text"
+                    placeholder='Looking for a recipe?'
+                    onChange={handleChange}/>
+                    </Form.Group>
+                </Form>
         </div>
     )
 }
